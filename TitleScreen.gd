e86bd6fe52a22545b7bfd13fff2940b93df04b25
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-03-28 21:07:11 +0100

    music plays

---
+++ TitleScreen.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+func _ready():
+    Sound.setup_music()
+    initialise_game_values()
+
+func initialise_game_values():
+    pass
+
+func _on_VolumeUp_pressed():
+    Sound.master_volume_up()
+
+func _on_VolumeDown_pressed():
+    Sound.master_volume_down()
+
+#foo
+#foo
+
+#foo x 3
