fa1221a89533fd27aaeb1625e2d4c8909217bd41
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-04-02 10:33:40 +0100

    fail state

--- autoload/story.gd
+++ autoload/story.gd
@@ -5,6 +5,10 @@ var boredom: int
 var disbelief: int
 var annoyance: int
 
+const boredom_threshold: int = 100
+const disbelief_threshold: int = 100
+const annoyance_threshold: int = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
@@ -20,3 +24,8 @@ func tell_story(boredom_effect, disbelief_effect, annoyance_effect):
     boredom += boredom_effect
     annoyance += annoyance_effect
     night += 1
+    
+    if (boredom > boredom_threshold
+        || disbelief > disbelief_threshold
+        || annoyance > annoyance_threshold):
+        Switcher.switch_scene("res://TitleScreen.tscn")
--- plot_button.gd
+++ plot_button.gd
@@ -4,10 +4,8 @@ export(int) var boredom_effect
 export(int) var disbelief_effect
 export(int) var annoyance_effect
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
-
 
 func _on_Button_pressed():
     Story.tell_story(boredom_effect, disbelief_effect, annoyance_effect)
