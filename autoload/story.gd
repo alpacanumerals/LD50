219e0f07fc02b73b947c08d6548ab68ecec5ddf9
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-04-02 13:06:39 +0100

    new backend with memory

--- autoload/story.gd
+++ autoload/story.gd
@@ -9,34 +9,30 @@ const boredom_threshold: int = 100
 const disbelief_threshold: int = 100
 const annoyance_threshold: int = 100
 
+var memory = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
 
 func initialise():
     night = 1
-    disbelief = 20
-    boredom = 20
-    annoyance = 20
+    memory = {}
 
-func tell_story(boredom_effect, disbelief_effect, annoyance_effect):
-    boredom += boredom_effect
-    disbelief += disbelief_effect
-    annoyance += annoyance_effect
+func tell_story(subject_id, verb_id, object_id):
     night += 1
-
-    bound_values()
+    
+    var story_key = subject_id * 10000 + verb_id * 100 + object_id
+    
+    var repeats = memory.get(story_key, 0)
+    memory.set(story_key, repeats+1)
+    
+    # handle logic based on repeats
+    
+    # handle logic based on story content
     
     if (threshold_exceeded()):
         Switcher.switch_scene("res://TitleScreen.tscn")
 
 func threshold_exceeded():
     return boredom > boredom_threshold || disbelief > disbelief_threshold || annoyance > annoyance_threshold
-
-func bound_values():
-    if (boredom < 0):
-        boredom = 0
-    if (disbelief < 0):
-        disbelief = 0
-    if (annoyance < 0):
-        annoyance = 0
