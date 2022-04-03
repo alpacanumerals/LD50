de16cbb44bc99e716581e08c82518a2ea2aa9aa1
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-04-03 23:40:32 +0100

    stab scene

--- story_scene.gd
+++ story_scene.gd
@@ -2,6 +2,7 @@ extends MarginContainer
 
 func _ready():
     $NightArea.initialise()
+    set_start_sprites()
 
 func _on_DoneButton_pressed():
     var subject = Story.current_tale[Story.phase.SUBJECT]
@@ -14,3 +15,19 @@ func _on_DoneButton_pressed():
 
 func _on_TimeLimit_time_up():
     $NightArea.night_fall(true)
+
+func set_start_sprites():
+    $Shez.visible = true
+    $Sultan.visible = true
+    $Shez2.visible = false
+    $Sultan2.visible = false
+    
+func set_end_sprites():
+    $Shez.visible = false
+    $Sultan.visible = false
+    $Shez2.visible = true
+    $Sultan2.visible = true
+
+
+func _on_NightArea_game_over():
+    set_end_sprites()
