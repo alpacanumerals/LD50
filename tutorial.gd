4792cd84c3a3d01c8268f4df8f7d92b9d1183291
alpacanumerals <72301530+alpacanumerals@users.noreply.github.com>
2022-04-04 13:01:57 +0100

    ugly tutorial

---
+++ tutorial.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var tutorial_on : bool = true
+
+func _process(_delta):
+    tutorial_on = Story.night == 1
+    if tutorial_on:
+        $Label0.visible = true
+        $Label1.visible = false
+        $Label2.visible = false
+        $Label3.visible = false
+        $Label4.visible = false
+        match Story.current_phase:
+            Story.phase.SUBJECT:
+                $Label1.visible = true
+            Story.phase.VERB:
+                $Label2.visible = true
+            Story.phase.OBJECT:
+                $Label3.visible = true
+            Story.phase.NONE:
+                $Label4.visible = true
+                
+    else:
+        $Label0.visible = false
+        $Label1.visible = false
+        $Label2.visible = false
+        $Label3.visible = false
+        $Label4.visible = false
