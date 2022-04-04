bf6a8a7f4ac1e7ebc5b1de96a47ef3f7bec16367
ruruie <58190249+Ruruie-FGSC@users.noreply.github.com>
2022-04-04 09:01:29 -0400

    Update sultanspeech.gd

--- autoload/sultanspeech.gd
+++ autoload/sultanspeech.gd
@@ -1,7 +1,157 @@
 extends Node
 
-
+var finalopinion = []
+var line
 
 func speak():
-    
-    pass
+    finalopinion.clear()
+    if Storyrules.offenses.has(Storyrules.rules.CAT_LIFE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.RESOURCEFUL_ESCAPEBURIAL):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TWIN_CONFUSED):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TWIN_SUBJECTASSIGNED):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TWIN_OBJECTASSIGNED):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TWIN_REVERSETRANSFORM):
+        
+    if Storyrules.offenses.has(Storyrules.rules.ACTION_DEAD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.ACTION_NONSENSE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_GENERIC):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_FICKLEWOMAN):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_SWORD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_MAGICWARRIORPRINCESS):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_SLAYHUGE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_SLEWWITHSWORD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_SLEWWITHMAGIC):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_SLAY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_THROW):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_TURNTOF):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CHAUVANIST_TURN):
+        
+    if Storyrules.offenses.has(Storyrules.rules.GENERIC_NONSENSE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.BURNED_CLEVER):
+        
+    if Storyrules.offenses.has(Storyrules.rules.BURNED_UNREASONABLE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.BURNED_HATELIST):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CLIMBED_OVEROVERSIZE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.CLIMBED_OVERSIZE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.DESPISED_CAT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.DESPISED_INCONSISTENT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_FROMNOWHERE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_CLEVER):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_DAMOCLES):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_ALREADY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_BURNINGSTRUCTURE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_RUBBLE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.FELL_CRUSHEDBY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.HAD_STRANGE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.LOVED_INCONSISTENT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.LOVED_CAT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_BOTHALREADY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_SUBJECTALREADY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_OBJECTALREADY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_BADMATCH):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_YURINO):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_YAOIMAYBE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_Y_IS_FINE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_POLITICAL):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_BOTHHATE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_LOVEGROSS):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_SAPPY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_NOBUILDUP):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_SPICY):
+        
+    if Storyrules.offenses.has(Storyrules.rules.REVIVED_CAT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.REVIVED_CATDEAD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.REVIVED_DEUSEX):
+        
+    if Storyrules.offenses.has(Storyrules.rules.REVIVED_USEMAGIC):
+        
+    if Storyrules.offenses.has(Storyrules.rules.REWARDED_DUBIOUS):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_ALREADY_DEAD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_UNKILLABLE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_COURTSPICE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_HIGHLYTOPICAL):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_UNSPICYREGICIDE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_STRANGE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_SUICIDE_CAT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_SUICIDE_KING):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_SUICIDE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SLEW_CAT):
+        
+    if Storyrules.offenses.has(Storyrules.rules.STOLE_HUGE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.STRUCK_HUGEDEAD):
+        
+    if Storyrules.offenses.has(Storyrules.rules.THREW_HUGE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.THREW_NOHANDS):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TURN_NOMAGIC):
+        
+    if Storyrules.offenses.has(Storyrules.rules.TURN_INTO):
+        
+    if Storyrules.offenses.has(Storyrules.rules.MARRIED_INANIMATE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.PURSUED_INANIMATE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.SPOKE_WITH_INANIMATE):
+        
+    if Storyrules.offenses.has(Storyrules.rules.KING_MISFORTUNE):
+    return finalopinion
